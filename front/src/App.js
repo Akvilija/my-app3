c4ec1a72910ebffddac9f929603cd9cb5a6f9344
Akvilija <akvilija.skorupskaite199503@gmail.com>
2025-01-22 19:56:35 +0200

    added styling and fixed some parts

--- front/src/App.js
+++ front/src/App.js
@@ -1,4 +1,3 @@
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import UsersPage from "./pages/UsersPage"; 
 import UserPage from "./pages/UserPage"; 
--- front/src/components/UserItem/UserItem.jsx
+++ front/src/components/UserItem/UserItem.jsx
@@ -1,12 +1,24 @@
+import "./UserItem.scss"
+import userLogo from "../../images/user.png"
 import { Link } from "react-router-dom";
 
-const UserItem = ({ user }) => {
+const UserItem = ({ user, onDelete }) => {
   return (
-    <div>
-      <h3>{user.name}</h3>
-      <p>{user.city || "No city available"}</p>
-      <Link to={`/users/${user._id}`}>View Details</Link>
+    <div className="user-item-container">
+    <h3 className="user-name">{user.name}</h3>
+    {user.city && <p className="user-city">{user.city}</p>}
+    <div className="user-and-logo">
+      <div className="logo-container">
+        <img alt="user-logo" src={userLogo} />
+      </div>
     </div>
+    <div className="actions">
+      <button onClick={() => onDelete(user._id)}>Delete</button>
+      <Link to={`/users/${user._id}`}>
+        <button className="details">View Details</button>
+      </Link>
+    </div>
+  </div>
   );
 };
 
